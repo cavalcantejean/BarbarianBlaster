66667f351d803fcb9fe8ad8580735e93279ed025
Jean Francisco Cavalcante <jean870@gmail.com>
2024-07-30 16:09:09 -0300

    Initial commit

---
+++ Enemy/enemy.gd
@@ -0,0 +1,13 @@
+extends PathFollow3D
+
+@export var speed: float = 2.5
+
+@onready var base = get_tree().get_first_node_in_group("base")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	progress += delta * speed
+	
+	if progress_ratio == 1.0:
+		base.take_demage()
+		set_process(false)
